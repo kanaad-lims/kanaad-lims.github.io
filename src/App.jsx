fb4c20842de036f471219d90de58555bb0559cf7
Kanaad Limaye <kanaad.limaye@gmail.com>
2026-09-12 18:39:02 +0530

    Added structural changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 
-import Aurora from "./components/background/Aurora";
-import DotField from "./components/background/DotField";
+import "./App.css";
 import Hero from "./Hero";
 import About from "./About";
 import Experience from "./Experience";
@@ -13,24 +12,20 @@ import Footer from "./Footer";
 
 function App() {
   return (
-    <div style={{ width: "100vw", height: "100vh" }}>
-      <Aurora />
-      <Hero />
-      <div style={{height: "50px"}}></div>
-      <About />
-      <div style={{height: "10px"}}></div>
-      <Experience />
-      <Projects />
-      <Research />
-      <Achievements />
-      <Leadership />
-      <Gallery />
-      <Footer />
-
+    <div className="app">
+      <main className="page-content">
+        <Hero />
+        <About />
+        <Experience />
+        <Projects />
+        <Research />
+        <Achievements />
+        <Leadership />
+        <Gallery />
+        <Footer />
+      </main>
     </div>
   );
 }
 
 export default App;
-
-
